ef91bdee499c6fcf4ef7752934f852f08a3270ee
SymbulF <apddef11@hotmail.com>
2016-12-17 14:34:43 -0200

    Aliencity_3D
    Level armadilhas

---
+++ AlienCity_Levels/Assets/Scripts/QuedaStalag2.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuedaStalag2 : MonoBehaviour {
+
+	public GameObject player;
+
+
+	void Start(){
+
+
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.gameObject.CompareTag ("Player")) 
+		{
+
+			GameObject.FindWithTag ("Estalags1").GetComponent<Rigidbody> ().useGravity = true;
+			GameObject.FindWithTag ("Estalags2").GetComponent<Rigidbody> ().useGravity = true;
+			GameObject.FindWithTag ("Estalags3").GetComponent<Rigidbody> ().useGravity = true;
+			GameObject.FindWithTag ("Estalags4").GetComponent<Rigidbody> ().useGravity = true;
+			GameObject.FindWithTag ("Estalags5").GetComponent<Rigidbody> ().useGravity = true;
+
+
+		}
+
+	}
+}
---
+++ AlienCity_Levels/Assets/Scripts/TocaEmArmad.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TocaEmArmad : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject camO;
+	public bool dead;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		player.SetActive (true);
+		camO.SetActive (false);
+		dead = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(dead == true && Input.GetKeyDown(KeyCode.R))
+		{
+			camO.SetActive (false);
+			player.SetActive (true);
+
+			Application.LoadLevel (Application.loadedLevel);			
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player")) 
+		{
+			player.SetActive (false);
+			camO.SetActive (true);
+			dead = true;
+
+		}
+
+	}
+}
